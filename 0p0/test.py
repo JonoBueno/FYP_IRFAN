cde9bedf0a28da1c163300064608f8a62b229a45
JonoBueno <irfanmuqri1@gmail.com>
2025-01-16 05:58:05 +0800

    uodate

--- 0p0/test.py
+++ 0p0/test.py
@@ -1,92 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-import os
-
-labels = [11,12,13,14,15,16,17,18]
-df = []
-df1 = []
-masses = []
-pred =float(input("What is the mass ="))
-
-
-
-for label in labels:
-
-    if label != pred:
-        masses += [label]
-        df += [pd.read_csv(f"./0p0/{label}.data", sep="\s+")]
-    df1 += [pd.read_csv(f"./0p0/{label}.data", sep="\s+")]
-masses = np.array(masses)
-
-ages = []
-lums = []
-ages1 = []
-lums1 = []
-for d in df:
-    condition = 10 * d["log_LH"] > 1 * 10 * d["log_L"]
-    if condition.any():
-        start_h_burning = d.loc[condition, "star_age"].iloc[0]
-    else:
-        start_h_burning = None
-
-    condition = d["center_h1"] < 1e-6
-    if condition.any():
-        end_h_burning = d.loc[condition, "star_age"].iloc[0]
-    else:
-        end_h_burning = None
-
-    d = d[(d["star_age"] > start_h_burning) & (d["star_age"] < end_h_burning)]
-
-    ages += [d["star_age"]]
-    lums += [d["log_Lneu"]]
-
-for d in df1:
-    condition = 10 * d["log_LH"] > 1 * 10 * d["log_L"]
-    if condition.any():
-        start_h_burning = d.loc[condition, "star_age"].iloc[0]
-    else:
-        start_h_burning = None
-
-    condition = d["center_h1"] < 1e-6
-    if condition.any():
-        end_h_burning = d.loc[condition, "star_age"].iloc[0]
-    else:
-        end_h_burning = None
-
-    d = d[(d["star_age"] > start_h_burning) & (d["star_age"] < end_h_burning)]
-
-    ages1 += [d["star_age"]]
-    lums1 += [d["log_Lneu"]]
-
-min_length = min(len(lum) for lum in lums)
-ages = np.log([age[:min_length] for age in ages])
-lums = np.array([lum[:min_length] for lum in lums])
-
-min_length = min(len(lum) for lum in lums1)
-ages1 = np.log([age[:min_length] for age in ages1])
-lums1 = np.array([lum[:min_length] for lum in lums1])
-
 from sklearn.linear_model import LinearRegression as LnR
 
-output = np.array(
-    [
-        *(i for z in zip(ages.T, lums.T) for i in z),
-    ]
-).T
-print(output.shape)
+x = np.arange(0, 50, 1)
+y = []
+
+for val in x:
+    y.append(val**2)
+
+plt.plot(x, y, label='Original Data')
+
+# Reshaping x and y to be 2D arrays
+x = x.reshape(-1, 1)
+y = np.array(y).reshape(-1, 1)
+
 model = LnR()
-model.fit(np.array([masses]).T, output)
+model.fit(x, y)
 
-prediction = model.predict([[pred]])
-predicted_ages = prediction[:, ::2]
-predicted_lums = prediction[:, 1::2]
-print(prediction.shape)
+# Predicting using the model
+predictions = model.predict(x).flatten()
 
-plt.plot(predicted_ages[0], predicted_lums[0], label=f"predicted {pred}")
-
-for i, label in enumerate(labels):
-    plt.plot(ages1[i], lums1[i], label=f"grid {label}")
-
-
-plt.legend(fontsize=6)
+plt.plot(x, predictions, label='Linear Fit', color='red')
+plt.xlabel('x');plt.ylabel('y')
+plt.title('Example of Linear Regression on Non-linear Data')
+plt.legend()
+plt.show()
